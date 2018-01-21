44a285866d9cefef0899005af28caec845f5c562
Maciej Sitko <maciejsitko@Maciejs-MacBook-Pro.local>
2018-01-21 13:29:59 +0000

    JS native extension missing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import EstimateHandler from './handlers/estimate'
+import EstimateHandler from './handlers/estimate.js'
 
 if(/website\/estimate[/]?$/.test(window.location.pathname)) {
     let _ = new EstimateHandler()
